4bdf8c88de76085e63562be3718cbe12cb5b8935
mainmesh <meshackmbithi254@gmail.com>
2025-04-14 14:36:20 +0300

    Update settings and procfile

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -202,8 +202,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('meshackmbithi01@gmail.com')
+EMAIL_HOST_PASSWORD = config('rhvn mxfe wunp xwcw')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_USE_SSL = False
 
