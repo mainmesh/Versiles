18336d3429b49398cf4d6808aecc4ba2025f7fbc
mainmesh <meshackmbithi254@gmail.com>
2025-07-05 07:24:28 +0300

    Fix ALLOWED_HOSTS for Render

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -14,7 +14,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG")
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['versiles-em39.onrender.com']
+
+
 
 # Application definition
 
